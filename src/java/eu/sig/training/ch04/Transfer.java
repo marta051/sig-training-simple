ce992457942433e4a686d5b8de637ed597e7a667
IT070717 <IT070717@NBR9P8H0HCH.corp.bankbtpn.co.id>
2017-10-10 14:10:50 +0700

    ch04-Transfer

--- src/java/eu/sig/training/ch04/Transfer.java
+++ src/java/eu/sig/training/ch04/Transfer.java
@@ -3,10 +3,8 @@ package eu.sig.training.ch04;
 public class Transfer {
     CheckingAccount counterAccount;
 
-    @SuppressWarnings("unused")
     public Transfer(CheckingAccount acct1, CheckingAccount acct2, Money m) {}
 
-    @SuppressWarnings("unused")
     public Transfer(SavingsAccount acct1, CheckingAccount acct2, Money m) {}
 
     public CheckingAccount getCounterAccount() {
