95d4c3b086be11ec0e2a081216ad46e0a726ff6d
IT070717 <IT070717@NBR9P8H0HCH.corp.bankbtpn.co.id>
2017-10-10 14:23:19 +0700

    ch04-accounts

--- src/java/eu/sig/training/ch04/Accounts.java
+++ src/java/eu/sig/training/ch04/Accounts.java
@@ -1,18 +1,18 @@
 package eu.sig.training.ch04;
 
 public class Accounts {
-    @SuppressWarnings("unused")
-    public static CheckingAccount findAcctByNumber(String number) {
-        return new CheckingAccount();
-    }
 
-    // tag::isValid[]
-    public static boolean isValid(String number) {
+    public Transfer makeTransfer(String counterAccount, Money amount) throws BusinessException {
         int sum = 0;
-        for (int i = 0; i < number.length(); i++) {
-            sum = sum + (9 - i) * Character.getNumericValue(number.charAt(i));
+        for (int i = 0; i < counterAccount.length(); i++) {
+            sum = sum + (9 - i) * Character.getNumericValue(counterAccount.charAt(i));
         }
-        return sum % 11 == 0;
+        if (sum % 11 == 0) {
+            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
+            Transfer result = new Transfer(this, acct, amount);
+            return result;
+        } else {
+            throw new BusinessException("Invalid account number!");
+        }
     }
-    // end::isValid[]
 }
