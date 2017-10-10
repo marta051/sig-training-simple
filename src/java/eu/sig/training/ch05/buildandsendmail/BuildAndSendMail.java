e5fb0965e31172cb65c1a5f9bd6b0b4a9a6c83c4
IT070717 <IT070717@NBR9P8H0HCH.corp.bankbtpn.co.id>
2017-10-10 14:34:12 +0700

    ch05-buildandsendEmail

--- src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
+++ src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
@@ -1,5 +1,4 @@
 
-
 import eu.sig.training.ch05.buildandsendmail.EmailContent;
 import eu.sig.training.ch05.buildandsendmail.Mail;
 import eu.sig.training.ch05.buildandsendmail.MailFont;
@@ -7,21 +6,16 @@ import eu.sig.training.ch05.buildandsendmail.MailMessage;
 import eu.sig.training.ch05.buildandsendmail.Person;
 
 public class BuildAndSendMail {
-    // tag::buildAndSendMail[]
-    public void buildAndSendMail(Mail mail, Person person,  EmailContent emailContent) {
-        // Format the email address
-        String mId = person.getFirstName().charAt(0) + "." + person.getLastName().substring(0, 7) + "@"
-            + person.getDivision().substring(0, 5) + ".compa.ny";
-        // Format the message given the content type and raw message
-        MailMessage mMessage = formatMessage(mail.getMailFont(),
-            emailContent.getMessage1() + emailContent.getMessage2()+ emailContent.getMessage3());
-        // Send message
-        mail.getMailMan().send(mId, emailContent.getSubject(), mMessage);
-    }
-    
-    // end::buildAndSendMail[]
-    private MailMessage formatMessage(MailFont font, String string) {
-        return null;
-    }
+	public void buildAndSendMail(Mail mail, Person person, EmailContent emailContent) {
+		String mId = person.getFirstName().charAt(0) + "." + person.getLastName().substring(0, 7) + "@"
+				+ person.getDivision().substring(0, 5) + ".compa.ny";
+		MailMessage mMessage = formatMessage(mail.getMailFont(),
+				emailContent.getMessage1() + emailContent.getMessage2() + emailContent.getMessage3());
+		mail.getMailMan().send(mId, emailContent.getSubject(), mMessage);
+	}
+
+	private MailMessage formatMessage(MailFont font, String string) {
+		return null;
+	}
 
 }
